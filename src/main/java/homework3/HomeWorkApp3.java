678e2fdbebe39eae0227fccda60f6f519ed1174d
DeadLik <95370475+DeadLik@users.noreply.github.com>
2022-01-04 04:28:25 +0300

    Update method =TwoDimensionalArray=

--- src/main/java/homework3/HomeWorkApp3.java
+++ src/main/java/homework3/HomeWorkApp3.java
@@ -77,7 +77,7 @@ public class HomeWorkApp3 {
     public static void TwoDimensionalArray(int[][] arr2) {
         for (int i = 0; i < arr2.length; i++) {
             for (int j = 0; j < arr2[i].length; j++) {
-                if (i == j) {
+                if (i == j || i + j == 4) {
                     arr2[i][j] = 1;
                 }
                 System.out.print(arr2[i][j] + " ");
