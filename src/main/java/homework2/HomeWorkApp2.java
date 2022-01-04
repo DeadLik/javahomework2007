07d8c94f99626b2800207accc1f6db372bb8a751
DeadLik <95370475+DeadLik@users.noreply.github.com>
2022-01-04 18:54:15 +0300

    Update HomeWorkApp2.java

--- src/main/java/homework2/HomeWorkApp2.java
+++ src/main/java/homework2/HomeWorkApp2.java
@@ -2,16 +2,16 @@ package homework2;
 
 public class HomeWorkApp2 {
     public static void main(String[] args) {
-        CheckPositNegBoolValue(5);
-        CheckPositNegValue(5);
-        CheckValue(6, 6);
-        TextAndNumber("Test", 5);
-        CheckYear(2009);
+        checkPositNegBoolValue(5);
+        checkPositNegValue(5);
+        checkValue(6, 6);
+        textAndNumber("Test", 5);
+        checkYear(2009);
 
     }
 
     // Задание №1
-    public static boolean CheckValue(int v1, int v2) {
+    public static boolean checkValue(int v1, int v2) {
 
         int result = v1 + v2;
 
@@ -21,7 +21,7 @@ public class HomeWorkApp2 {
     }
 
     // Задание №2
-    public static void CheckPositNegValue(int x1) {
+    public static void checkPositNegValue(int x1) {
         if (x1 < 0) {
             System.out.println("Число отрицательное");
         } else {
@@ -30,12 +30,12 @@ public class HomeWorkApp2 {
     }
 
     // Задание №3
-    public static boolean CheckPositNegBoolValue(int a1) {
+    public static boolean checkPositNegBoolValue(int a1) {
         return a1 < 0;
     }
 
     // Задание №4
-    public static void TextAndNumber(String Text, int b) {
+    public static void textAndNumber(String Text, int b) {
         for (int i = 0; i < b; i++) {
             System.out.println(Text);
         }
@@ -43,7 +43,7 @@ public class HomeWorkApp2 {
 
     //Задание со *, Долго пытался понять в чем загвоздка, вроде победил
 
-    public static boolean CheckYear(int year) {
+    public static boolean checkYear(int year) {
         if (year % 4 == 0 || year % 400 == 0 && year != 100) {
             return true;
         } else return false;
